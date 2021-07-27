5e90b5c19d69540ae0d9042b1829ba2639832a92
sunlight-nadezhda <75030781+sunlight-nadezhda@users.noreply.github.com>
2021-07-27 20:48:29 +0300

    feat: создать схемы и модели пользователя и фильма

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
-
-const { PORT = 3000 } = process.env;
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.listen(PORT, () => {});
+mongoose.connect('mongodb://localhost:27017/moviesdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.listen(3000);
---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(v) {
+        return validator.isEmail(v);
+      },
+      message: 'Введите корректный email',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+},
+{
+  versionKey: false,
+});
+
+module.exports = mongoose.model('user', userSchema);
